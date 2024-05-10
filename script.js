3c9df0dc83c10bec3ee184d1496b0dc70c924cd5
hunter-h25 <hunterhargett519@outlook.com>
2024-05-09 21:41:08 -0500

    Final commmit

--- script.js
+++ script.js
@@ -4,9 +4,7 @@ function handleSubmit(event) {
     const email = document.getElementById('email').value;
     const message = document.getElementById('message').value;
 
-    // Here you can process the form data as desired, for example, sending it to a server or logging it
     console.log(`Name: ${name}\nEmail: ${email}\nMessage: ${message}`);
 
-    // For demonstration purposes, just alert the user
     alert("Form submitted successfully!");
 }
